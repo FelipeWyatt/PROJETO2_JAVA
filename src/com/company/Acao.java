686e0f0d9dbfef0849889318e600eeb713f34e5c
Felipe Wyatt Varga <57908903+FelipeWyatt@users.noreply.github.com>
2021-01-05 16:42:42 -0300

    Função rende()
    Adicionei a nova função que era abstrata na classe mãe.

--- src/com/company/Acao.java
+++ src/com/company/Acao.java
@@ -7,5 +7,10 @@ public class Acao extends Investimento {
     public Acao(float montante, GregorianCalendar dataCompra) {
         super(montante, dataCompra);
     }
+    
+    public void rende(int diasPassados){
+           // Exemplo de método
+           setMontante(getMontante()*2);
+    }
 
 }
