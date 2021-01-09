011f47f32f9ebd09d43b05a13f8f94b317e72cb2
Matheus02Meirelles <77208037+Matheus02Meirelles@users.noreply.github.com>
2021-01-09 15:40:35 -0300

    Update Investimento.java
    Adição da Interface Comparable

--- src/com/company/Investimento.java
+++ src/com/company/Investimento.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.text.DecimalFormat;
 import java.util.GregorianCalendar;
 
-public class Investimento implements Serializable { // Classe mae para qualquer tipo de investimento, no futuro serao adicionados outros tipos alem de RendaFixa
+public class Investimento implements Serializable, Comparable<Investimento> { // Classe mae para qualquer tipo de investimento, no futuro serao adicionados outros tipos alem de RendaFixa
 	// ATRIBUTOS
 	private static final long serialVersionUID = 302L;
 	private float montante; // quanto foi investido
@@ -38,5 +38,19 @@ public class Investimento implements Serializable { // Classe mae para qualquer 
 	public void setDataCompra(GregorianCalendar dataCompra) {
 		this.dataCompra = dataCompra;
 	}
+	
+	//DA INTERFACE COMPARABLE
+	public int compareTo(Investimento outro) {
+		//SE TEM MAIS MONTANTE
+		if (this.montante > outro.montante) {
+			return -1;
+		}
+		//SE TEM MENOS MONTANTE
+		else if (this.montante < outro.montante) {
+			return 1;
+		}
+		//SE TEM O MESMO MONTANTE
+		return 0;
+	}	
 
 }
